8d16674d48dddd56da3e82e70016c2dd8be1b23b
djl <dujianglin17@mails.ucas.ac.cn>
2021-11-10 00:52:24 +0800

    simple push

--- http.c
+++ http.c
@@ -4,6 +4,8 @@
 #define URL_SZ 1024
 #define VERSION_SZ 16
 
+#define BUFFER_SZ 16*1024
+
 typedef struct http_header_t {
     char method[METHOD_SZ];
     char url[URL_SZ];
@@ -19,3 +21,18 @@ int get_method(char *method)
     else return -1;
 }
 
+int handle_request(int sock) {
+    char *buffer = malloc(BUFFER_SZ);
+    memset(buffer, BUFFER_SZ, 0);
+    read(sock, buffer, BUFFER_SZ);
+
+    char *data_buf = strstr(buffer, "\r\n\r\n");
+    data_buf = data_buf + 4;
+    char *head_buf = strtok(buffer, "\r\n\r\n");
+
+    char *sta_head = strstr(head_buf, "\r\n");
+    sta_head = sta_head + 2;
+    
+
+
+}
